4549950cd07bc974a976844c5424b0e641f84540
Dilyn Fullerton <dilyn.fullerton@att.net>
2016-02-18 18:22:42 -0800

    Numerical optimizer of A prescription

--- src/main.py
+++ src/main.py
@@ -5,20 +5,27 @@ from __future__ import unicode_literals
 from sys import path
 from itertools import combinations_with_replacement, permutations
 
+import numpy as np
 from matplotlib import pyplot as plt
+from scipy.optimize import leastsq
 
 from toy import HamiltonianToy as H_ex
 from toy import HamiltonianToyEffective as H_eff
 from toy import get_a_exact as exact
 from toy import custom_a_prescription as custom
 
-path.append(b'../../imsrg_mass_plots/src')
+path.extend([b'../../imsrg_mass_plots/src'])
 # noinspection PyPep8,PyUnresolvedReferences
 from plotting import plot_the_plots
+from metafit import _meta_fit
 
 A_PRESCRIPTIONS = [exact,
                    custom(4, 5, 6),
-                   custom(6, 6, 6)
+                   custom(6, 6, 6),
+                   custom(6, 7, 8),
+                   custom(7, 7, 8),
+                   custom(7, 7, 7),
+                   custom(6.79, 7.14, 7.54)
                    # custom(2, 3, 4),
                    # custom(4, 4, 4)
                    ]
@@ -29,10 +36,10 @@ VALENCE_SPACE = range(5, 18)
 HW = 1
 V0 = 1
 T_2 = 0
-T_CORE = T_2
-T_MIX = T_2
-T_VAL = T_2
-LATEX = True
+T_CORE = 0
+T_MIX = -1
+T_VAL = 0
+LATEX = False
 
 
 def plot_a_prescriptions(a_prescriptions=A_PRESCRIPTIONS,
@@ -68,34 +75,14 @@ def plot_a_prescriptions(a_prescriptions=A_PRESCRIPTIONS,
         else:
             const_dict = {'presc': ap(0)[4]}
         for k in k_range:
-            a2, a3, a4 = ap(k)[:3]
-
-            h2 = H_ex(a=a2, v0=v0, hw=hw, valence_space=valence_space,
-                      t_core=t_core, t_mix=t_mix, t_val=t_val)
-            h3 = H_ex(a=a3, v0=v0, hw=hw, valence_space=valence_space,
-                      t_core=t_core, t_mix=t_mix, t_val=t_val)
-            h4 = H_ex(a=a4, v0=v0, hw=hw, valence_space=valence_space,
-                      t_core=t_core, t_mix=t_mix, t_val=t_val)
-
-            e2 = h2.ground_state_energy(k=valence_space[0] - 1)
-            e3 = h3.ground_state_energy(k=valence_space[0])
-            e4 = h4.ground_state_energy(k=valence_space[0] + 1)
-
-            e_core = e2
-            e_p = e3 - e2
-            v_eff = e4 - 2 * e3 + e2
-            h_eff = H_eff(e_core=e_core, e_p=e_p, v_eff=v_eff,
-                          valence_space=valence_space)
-            h_exact = H_ex(a=k, v0=v0, hw=hw, valence_space=valence_space,
-                           t_core=t_core, t_mix=t_mix, t_val=t_val)
-            e_eff = h_eff.ground_state_energy(k)
-            e_exact = h_exact.ground_state_energy(k)
-
+            err = _get_e_eff_error(
+                ap=ap, k=k, valence_space=valence_space,
+                v0=v0, hw=hw, t_core=t_core, t_mix=t_mix, t_val=t_val)
             x.append(k)
             if hw != 0:
-                y.append((e_eff - e_exact) / hw)
+                y.append(err / hw)
             else:
-                y.append((e_eff - e_exact))
+                y.append(err)
         plots.append((x, y, const_list, const_dict))
 
     if not use_latex:
@@ -128,6 +115,52 @@ def plot_a_prescriptions(a_prescriptions=A_PRESCRIPTIONS,
         dark=False)
 
 
+def e_eff_error_array(a_prescription, k_range, valence_space, v0, hw,
+                      t_core, t_mix, t_val):
+    x_array = np.array(k_range)
+    y_array = np.empty(shape=x_array.shape)
+    for x, i in zip(x_array, range(len(x_array))):
+        ap = custom(*a_prescription)
+        y_array[i] = _get_e_eff_error(
+            ap=ap, k=x, valence_space=valence_space,
+            v0=v0, hw=hw, t_core=t_core, t_mix=t_mix, t_val=t_val)
+    return y_array
+
+
+def e_eff_error_arrays(a_prescription, params_range):
+    list_of_y_array = list()
+    for params in params_range:
+        list_of_y_array.append(e_eff_error_array(a_prescription, *params))
+    return np.concatenate(tuple(list_of_y_array))
+
+
+def _get_e_eff_error(ap, k, valence_space, v0, hw, t_core, t_mix, t_val):
+    a2, a3, a4 = ap(k)[:3]
+
+    h2 = H_ex(a=a2, v0=v0, hw=hw, valence_space=valence_space,
+              t_core=t_core, t_mix=t_mix, t_val=t_val)
+    h3 = H_ex(a=a3, v0=v0, hw=hw, valence_space=valence_space,
+              t_core=t_core, t_mix=t_mix, t_val=t_val)
+    h4 = H_ex(a=a4, v0=v0, hw=hw, valence_space=valence_space,
+              t_core=t_core, t_mix=t_mix, t_val=t_val)
+
+    e2 = h2.ground_state_energy(k=valence_space[0] - 1)
+    e3 = h3.ground_state_energy(k=valence_space[0])
+    e4 = h4.ground_state_energy(k=valence_space[0] + 1)
+
+    e_core = e2
+    e_p = e3 - e2
+    v_eff = e4 - 2 * e3 + e2
+    h_eff = H_eff(e_core=e_core, e_p=e_p, v_eff=v_eff,
+                  valence_space=valence_space)
+    h_exact = H_ex(a=k, v0=v0, hw=hw, valence_space=valence_space,
+                   t_core=t_core, t_mix=t_mix, t_val=t_val)
+    e_eff = h_eff.ground_state_energy(k)
+    e_exact = h_exact.ground_state_energy(k)
+
+    return e_eff - e_exact
+
+
 def permutations_with_replacement(iterable, r):
     combos = combinations_with_replacement(iterable, r)
     perms = list()
@@ -136,6 +169,17 @@ def permutations_with_replacement(iterable, r):
     return set(perms)
 
 
-for t1, t2, t3 in sorted(permutations_with_replacement(range(2), 3)):
-    plot_a_prescriptions(t_core=t1, t_mix=t2, t_val=t3)
-    plt.show()
+# for t1, t2, t3 in reversed(sorted(permutations_with_replacement(range(3), 3))):
+#     plot_a_prescriptions(t_core=t1, t_mix=t2, t_val=t3)
+# plot_a_prescriptions(t_core=T_CORE, t_mix=T_MIX, t_val=T_VAL)
+for tt2 in np.linspace(-5.0, 0.0, 11):
+    plot_a_prescriptions(t_mix=tt2)
+plt.show()
+
+# params_range = list()
+# for t2 in np.linspace(-5.0, 0.0, 11):
+#     params_range.append((K_RANGE, VALENCE_SPACE, V0, HW, T_CORE, t2, T_VAL))
+# res = leastsq(func=e_eff_error_arrays, x0=np.array([1.0, 1.0, 1.0]),
+#               args=(params_range,),
+#               full_output=False)
+# print(res)
